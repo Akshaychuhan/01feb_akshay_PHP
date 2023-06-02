7081b894cf234797eaf6525416a32262c8cabadb
Akshaychuhan <122368312+Akshaychuhan@users.noreply.github.com>
2023-06-02 17:34:39 +0530

    Add files via upload

---
+++ assignment/module:3/module:3.1/circle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    float pie;
+    float area;
+    float radius;
+    pie = 3.14;
+    printf("Enter Radius of Circle : ");
+    scanf("%f",&radius);
+    area =pie*radius*radius; n
+    printf("Total Area of Circle is :%f",area);
+}
