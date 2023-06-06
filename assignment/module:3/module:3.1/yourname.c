332c7dedb4c3aa45ae73974ac86c27474626f7f3
Akshaychuhan <122368312+Akshaychuhan@users.noreply.github.com>
2023-06-06 17:22:50 +0530

    Add files via upload

---
+++ assignment/module:3/module:3.1/yourname.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a,b,c,d;
+    printf("enter your name:");
+    scanf("%d",a);
+    printf("\center your birth date:");
+    scanf("%d",b);
+    printf("\nenter your age:");
+    scanf("%d",c);
+    printf("\nenter your address:");
+    scanf("%d",d);
+
+
+}
