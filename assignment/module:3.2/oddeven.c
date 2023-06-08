4dcf1530eacb9e406325fb28d19aed161edde83c
Akshaychuhan <122368312+Akshaychuhan@users.noreply.github.com>
2023-06-08 17:55:29 +0530

    Add files via upload

---
+++ assignment/module:3.2/oddeven.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+        int a;
+    printf("enter your number:");
+    scanf("%d",&a);
+    if (a%=2)
+    {
+        printf("this number is odd:");
+    }
+    else
+
+    {
+        printf("this number is even:");
+        
+    }
+
+    
+}
