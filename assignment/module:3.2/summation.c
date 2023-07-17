5405dfa11e154cd63c00d8f7ff78824a17c81fcd
Akshaychuhan <122368312+Akshaychuhan@users.noreply.github.com>
2023-07-17 11:10:38 +0530

    Add files via upload

---
+++ assignment/module:3.2/summation.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int number,a,b;
+    printf("Enter Number : ");
+    scanf("%d",&number);
+    b= 0;
+    for ( int i = 1; number%10>0; i++)
+    {
+        a = number % 10;
+        
+        b = b+a;
+        number = number/10;
+    }
+    printf("Summation Of Given number is : %d",b);
+    return 0;
+}
