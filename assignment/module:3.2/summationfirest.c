034f63ee75a0dc2bc16ad9e086314272a4df7b76
Akshaychuhan <122368312+Akshaychuhan@users.noreply.github.com>
2023-07-17 11:13:48 +0530

    Add files via upload

---
+++ assignment/module:3.2/summationfirest.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int number,ans,fn,ln;
+    printf("Enter Numbers  :");
+    scanf("%d",&number);
+    ln = number % 10;
+    while ( number >= 10)
+    {
+        number = number/ 10;
+    }
+    fn = number; 
+    ans = fn + ln;
+    printf("Sum of First & Last digit is : %d",ans);
+
+
+
+}
